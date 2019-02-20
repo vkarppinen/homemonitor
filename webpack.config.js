3efbec200e90b95b6d4c345761303344e4412aea
Valtteri Karppinen <karppinenvaltteri@gmail.com>
2019-02-20 20:19:54 +0200

    set up basic webpack development

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const BUILD_PATH = path.join(__dirname, 'dist');
+const SOURCE_PATH = path.join(__dirname, 'src');
+
+module.exports = {
+    entry: path.join(SOURCE_PATH, 'index.jsx'),
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.join(SOURCE_PATH, 'index.html'),
+        filename: path.join(BUILD_PATH, 'index.html')
+      })
+    ],
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
